f80824a27d0fa4b84286f18a64e01783f49bf807
nitodeco <n.moehn@outlook.com>
2024-02-22 18:07:24 +0100

    feat: Implemented project config script

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
-# Kettleflame
+# Virety - (Vi)te + (Re)act + (Ty)pescript
 
-Just some boilerplate React+Vite+Typescript that I use for my projects.
+Just some boilerplate React+Vite+Typescript that I use for my projects. Feel free to use according to the [License](#License)
+
+## Usage
+
+1. **Clone repository:**
+
+```bash
+git clone https://github.com/nitodeco/virety
+```
+
+2. **Navigate to the Project Directory:**
+
+   - Once the cloning process is complete, navigate to the directory of the cloned project using the `cd` command. For example:
+     ```
+     cd example-project
+     ```
+
+3. **Install Node.js Dependencies:**
+
+   - Inside the project directory, there should be a `package.json` file which lists all the Node.js dependencies required for the project.
+   - Run the following command to install these dependencies using npm:
+     ```
+     npm install
+     ```
+   - This command will download and install all the dependencies listed in the `package.json` file.
+
+4. **Verify Installation:**
+
+   - After the installation process completes, you can verify that all dependencies were installed successfully by checking for any error messages in the terminal.
+   - Additionally, you can also check the `node_modules` directory within the project to ensure that all dependencies were downloaded.
+
+5. **Start Using the Project:**
+   - Once all dependencies are installed successfully, you can start using the project according to its documentation or instructions provided by the project maintainers.
---
+++ package-config.js
@@ -0,0 +1,70 @@
+import fs from 'fs'
+import readline from 'readline'
+import { execSync } from 'child_process'
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+// Fetch default author from Node.js configuration
+let author
+try {
+  author = execSync('npm config get init-author-name', {
+    encoding: 'utf-8',
+  }).trim()
+} catch (error) {
+  console.error('Error fetching default author:', error.message)
+  rl.close()
+}
+
+rl.question(`Author (${author}): `, (newAuthor) => {
+  author = newAuthor.trim() !== '' ? newAuthor : author
+
+  rl.question(`Project name: `, (newName) => {
+    rl.question(`License: `, (license) => {
+      console.log('Configuring project...')
+      // Read the package.json file
+      fs.readFile('package.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error('Error reading package.json:', err)
+          rl.close()
+          return
+        }
+
+        // Parse package.json data
+        let packageJson
+        try {
+          packageJson = JSON.parse(data)
+        } catch (jsonErr) {
+          console.error('Error parsing package.json:', jsonErr)
+          rl.close()
+          return
+        }
+
+        // Update the name field with the new project name
+        packageJson.name = newName
+
+        // Update the author field with the default author
+        packageJson.author = author
+
+        // Update the license field with the provided license
+        packageJson.license = license
+
+        // Write the updated package.json back to file
+        fs.writeFile(
+          'package.json',
+          JSON.stringify(packageJson, null, 2),
+          (writeErr) => {
+            if (writeErr) {
+              console.error('Error writing package.json:', writeErr)
+            } else {
+              console.log('Done.')
+            }
+            rl.close()
+          }
+        )
+      })
+    })
+  })
+})
