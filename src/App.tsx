7b52acf9859fb3bf1e407445b6005178be3742d4
nitodeco <n.moehn@outlook.com>
2024-02-23 02:39:07 +0100

    style: Updated template styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import ViteLogo from '@assets/icons/vite.svg'
+import '@styles/template.scss'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <img src={ViteLogo} alt="" id="vite-logo" />
       <h1>Virety</h1>
       <h2>Vite + React + Typescript</h2>
       <div>
@@ -13,6 +16,13 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+        <p>
+          Visit{' '}
+          <a href="https://github.com/nitodeco/virety" target="_blank">
+            Github
+          </a>{' '}
+          for more information
         </p>
       </div>
     </>
